ebdc3f3e18230bd1e72fe868fbc40715db2a3585
Hamid Jamal <hamidjamal@hotmail.co.uk>
2025-08-05 17:16:14 +0100

    refraction + FOV

--- camera.h
+++ camera.h
@@ -1,11 +1,11 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include "colour.h"
 #include "hittable.h"
 #include "ray.h"
 #include "rtweekend.h"
 #include "vec3.h"
+#include <cmath>
 #include <iostream>
 #include "material.h"
 
@@ -15,6 +15,14 @@ public:
   int image_width = 100;
   int samples_per_pixel = 10;
   int max_depth = 10;
+
+  double vfov = 90;
+  point3 lookfrom = point3(0,0,0);
+  point3 lookat = point3(0,0,-1);
+  vec3 vup = vec3(0,1,0);
+
+  double defocus_angle = 0;
+  double focus_dist = 10;
 
   void render(const hittable &world) {
     initialise();
@@ -43,6 +51,11 @@ private:
   point3 pixel00_loc;
   vec3 pixel_delta_u;
   vec3 pixel_delta_v;
+  vec3 u, v, w;
+  vec3 defocus_disk_u;
+  vec3 defocus_disk_v;
+
+
   void initialise() {
     image_height = int(image_width / aspect_ratio);
     // If lower than 1, set as 1
@@ -50,17 +63,25 @@ private:
 
     pixel_sample_scale = 1.0 / samples_per_pixel;
 
-    centre = point3(0, 0, 0);
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
+    centre = lookfrom;
+
+    auto theta = deg_to_rad(vfov);
+    auto h = std::tan(theta / 2);
+
+    auto viewport_height = 2.0 * h * focus_dist;
     auto viewport_width =
         viewport_height * (double(image_width) / image_height);
+
+    // Calculate the u, v, w,unit basis vectors for the camera coordinate frame
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
 
     // Caclulate the vectors across the horizontal and down the verticle
     // viewpoint
     // edges
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    auto viewport_u = viewport_width * u;
+    auto viewport_v = viewport_height * -v;
 
     // Calculate the horizontal and verticle delta vectors from pixel to pixel
     pixel_delta_u = viewport_u / image_width;
@@ -68,20 +89,25 @@ private:
 
     // Calculate upper left pixel location
     auto viewpoint_upper_left =
-        centre - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        centre - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
 
     pixel00_loc = viewpoint_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Calculate the camera defocus disk basis vector
+    auto defocus_radius = focus_dist * std::tan(deg_to_rad(defocus_angle / 2));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
   }
 
   ray get_ray(int j, int i) const {
-    // Construct a cmera ray originating from orig and directed at randomly
+    // Construct a cmera ray originating from the defocus diskand directed at randomly
     // samped point around pixel location i, j
 
     auto offset = sample_square();
     auto pixel_sample = pixel00_loc + ((j + offset.x()) * pixel_delta_u) +
                         ((i + offset.y()) * pixel_delta_v);
 
-    auto ray_orig = centre;
+    auto ray_orig = (defocus_angle <= 0) ? centre : defocus_disk_sample();
     auto ray_dir = pixel_sample - ray_orig;
 
     return ray(ray_orig, ray_dir);
@@ -91,6 +117,12 @@ private:
     // returns the vector to a random opint in the [-.5,-.5]-[+.5,+.5] unit
     // square
     return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+  }
+
+  point3 defocus_disk_sample() const {
+    // Returns a random point in the camera defocus disk
+    auto p = random_in_unit_disk();
+    return centre + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
   }
 
   colour ray_colour(const ray &r, int depth, const hittable &world) const {
--- material.h
+++ material.h
@@ -2,7 +2,9 @@
 #define MATERIAL_H
 
 #include "hittable.h"
+#include "ray.h"
 #include "vec3.h"
+#include <cmath>
 
 class material {
 public:
@@ -53,5 +55,45 @@ private:
   double fuzz;
 };
 
+class dielectric : public material {
+  public:
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered) const override {
+      attenuation = colour(1.0, 1.0, 1.0);
+      double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+      vec3 unit_dir = unit_vector(r_in.direction());
+      vec3 refracted = refract(unit_dir, rec.normal, ri);
+
+      double cos_theta = std::fmin(dot(-unit_dir, rec.normal), 1.0);
+      double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+      vec3 dir;
+
+      if(ri * sin_theta > 1.0) {
+        // Must reflect
+        dir = reflect(unit_dir, rec.normal);
+      } else {
+        // can reflect
+        dir = refract(unit_dir, rec.normal, ri);
+      }
+
+      scattered = ray(rec.p, dir);
+      return true;
+    }
+
+  private:
+    // Refreactive index in a vaccume or air or the ratio of material's refreactive
+    // index over thr refeactive index of enclosing media
+    double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+      // Using Schlick's approx
+
+      auto r0 = (1-refraction_index) / (1 + refraction_index);
+      r0 = r0 * r0;
+      return r0 + (1-r0)*std::pow((1-cosine), 5);
+    }
+  };
 
 #endif
